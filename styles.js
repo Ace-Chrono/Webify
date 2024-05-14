9a05c703fac897223433e7efbb03cd6d59b108dc
Ace-Chrono <richardszhao@gmail.com>
2024-05-13 22:40:40 -0400

    small modification

--- styles.js
+++ styles.js
@@ -1,5 +1,15 @@
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
+
+// Function to change heading background color
+function changeHeadingBg(color){
+    document.getElementById("heading").style.background = color;
+}
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === 'changeColor') {
-        document.body.style.backgroundColor = message.color;
+        changeBodyBg(message.color)
+        changeHeadingBg(message.color)
     }
 });
